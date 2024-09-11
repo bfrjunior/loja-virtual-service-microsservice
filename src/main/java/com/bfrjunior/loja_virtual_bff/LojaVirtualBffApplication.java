a584d4db2664046e8abb8bc82c43a4cfae552f84
Fernando Rocha <beneditofernandor@gmail.com>
2024-09-10 21:58:35 -0300

    initial commit

---
+++ src/main/java/com/bfrjunior/loja_virtual_bff/LojaVirtualBffApplication.java
@@ -0,0 +1,13 @@
+package com.bfrjunior.loja_virtual_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualBffApplication.class, args);
+	}
+
+}
